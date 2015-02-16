b0cc63e02e36f91f4e2ba3b5fa269ecf21bd8c9a
Aluriak <lucas.bourneuf@laposte.net>
2015-02-16 15:47:54 +0100

    final total not printed when only 1 dice

--- munin/functionnalities/dice_launcher/dice_launcher.py
+++ munin/functionnalities/dice_launcher/dice_launcher.py
@@ -47,15 +47,21 @@ class DiceLauncher(Functionnality):
         """Execute command for bot (unused), according to regex matchs (used) and sudo mode (unused)"""
         results = ''
         total   = 0
+        dice_set_count = 0
 
         for dice_set in matched_groups[0].split(' '):
             n, s = dice_set.lower().split('d')
             if '0' in (n, s): continue
             numbers  = [randint(1, int(s)) for _ in range(int(n))]
             total   += sum(numbers)
-            results += '{' + ', '.join((str(_) for _ in numbers)) + '} \ttotal: ' + str(sum(numbers)) + '\n'
+            results += ('{' 
+                        + ', '.join((str(_) for _ in numbers)) 
+                        + '} \ttotal: ' + str(sum(numbers)) 
+                        + '\n'
+                       )
+            dice_set_count += 1
 
-        return results + 'total: ' + str(total)
+        return results + ('total: ' + str(total) if dice_set_count > 1 else '')
 
 
 
