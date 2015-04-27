8e22261fa4d52d35658aef60ac558809ff53b168
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2015-04-27 18:18:20 +0200

    debug, tests, more regex

--- munin/control/control.py
+++ munin/control/control.py
@@ -20,6 +20,7 @@ import munin.functionnalities as fnct
 #########################
 RGX_SUDOERS_ADD = re.compile(r" *sudoers add *([a-zA-Z0-9_]*) *")
 RGX_DISCONNECT  = re.compile(r" *(quit|:q) *")
+RGX_SPEAK       = re.compile(r" *say (.*)")
 
 
 
@@ -39,17 +40,24 @@ class Control():
         self.bot, self.finished = bot, False
         self.commands = {
             RGX_SUDOERS_ADD: self.__bot_add_sudoer,
-            RGX_DISCONNECT: self.__bot_disconnect,
+            RGX_DISCONNECT : self.__bot_disconnect,
+            RGX_SPEAK      : self.__bot_speak,
         }
         # launch bot as thread
         self.bot_thread = threading.Thread(target=self.bot.start)
         self.bot_thread.start()
 
         # Initial functionnalities
-        self.available_functionnalities = [f for f in (getattr(fnct, f) for f in fnct.__dir__())
-                                           if callable(f) and isinstance(f(), fnct.Functionnality) 
-                                           and f.__class__ is not fnct.Functionnality]
+        self.available_functionnalities = [f for f in (getattr(fnct, a) for a in fnct.__dir__())
+                                           if callable(f) and issubclass(f, fnct.Functionnality) 
+                                           # and f.__class__ is not fnct.Functionnality
+                                           # and 'functionnality' not in f.__class__.__name__.lower()
+                                           and 'functionnality' not in f.__name__.lower()
+                                           # and f.__class__.__name__ != fnct.Functionnality.__name__
+                                           # and 'functionnality' not in super(f).__class__.__name__.lower()
+                                          ]
         for f in self.available_functionnalities:
+            print(f.__name__, f.__class__, super(f))
             self.bot.add_functionnality(f())
 
         # main control buckle
@@ -68,9 +76,10 @@ class Control():
 # PUBLIC METHODS ##############################################################
     def do_command(self, msg):
         """Operate given message as command"""
+        print('CMD:', msg)
         for rgx, cmd in self.commands.items():
             if rgx.match(msg):
-                print('Understood !')
+                print('Understood !', cmd)
                 cmd(rgx.findall(msg))
 
 
@@ -78,6 +87,7 @@ class Control():
     def __bot_add_sudoer(self, regex_result):
         """add asked sudoer to bot sudoers list"""
         print(regex_result)
+        print('not implemented !')
         #self.bot.add_sudoer(regex_result[0][0])
 
 
@@ -86,6 +96,11 @@ class Control():
         self.bot.disconnect()
         self.finished = True
 
+
+    def __bot_speak(self, regex_result):
+        """print message in canal"""
+        self.bot.send_message(regex_result[0])
+        print('Said:', regex_result[0])
 
 # PREDICATS ###################################################################
 # ACCESSORS ###################################################################
