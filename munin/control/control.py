2b6ec787a5b94765ac3fb2013c097e7abe3b3ac5
Aluriak <lucas.bourneuf@laposte.net>
2015-04-28 20:41:22 +0200

    fix typo + new features about dynamic import of plugins

--- munin/control/control.py
+++ munin/control/control.py
@@ -10,22 +10,26 @@
 import threading
 import re
 
-import munin.config
+import munin.config as config
 
 
 
 #########################
 # PRE-DECLARATIONS      #
 #########################
-LOGGER          = munin.config.logger()
+LOGGER          = config.logger()
 RGX_SUDOERS_ADD = re.compile(r"\s*sudoers\s+add\s*([a-zA-Z0-9_]*)\s*")
 RGX_DISCONNECT  = re.compile(r"\s*(quit|:q)\s*")
 RGX_SPEAK       = re.compile(r"\s*say\s(.*)")
+RGX_PLUGINS     = re.compile(r"\s*(plugins?|pl?g)\s(.*)")
+
+# PRINTINGS
+PRINTINGS_PLUGINS_MAX_WIDTH = 20
 
 
 
 #########################
-# CLASS                 #
+# CONTROL CLASS         #
 #########################
 class Control():
     """
@@ -39,10 +43,12 @@ class Control():
     def __init__(self, bot):
         self.bot, self.finished = bot, False
         self.commands = {
-            RGX_SUDOERS_ADD: self.__bot_add_sudoer,
-            RGX_DISCONNECT : self.__bot_disconnect,
-            RGX_SPEAK      : self.__bot_speak,
+            RGX_SUDOERS_ADD : self.__bot_add_sudoer,
+            RGX_DISCONNECT  : self.__bot_disconnect,
+            RGX_SPEAK       : self.__bot_speak,
+            RGX_PLUGINS     : self.__plugins,
         }
+
         # launch bot as thread
         self.bot_thread = threading.Thread(target=self.bot.start)
         self.bot_thread.start()
@@ -69,6 +75,8 @@ class Control():
                     print("?>", end='')
                     cmd = input('')
         except KeyboardInterrupt:
+            self.bot.disconnect()
+        except EOFError:
             self.bot.disconnect()
 
         LOGGER.info('Disconnected !')
@@ -104,6 +112,60 @@ class Control():
         """print message in canal"""
         self.bot.send_message(regex_result[0])
         LOGGER.debug('Said:' + str(regex_result[0]))
+
+
+    def __plugins(self, regex_result):
+        """management of plugins"""
+        command = regex_result[0][1].split(' ')
+        command, values = command[0], command[1:]
+        error_code = {
+            'name'  : 'need a valid plugin name',
+            'id'    : 'need a valid plugin index',
+        }
+        error = None
+
+        # listing
+        if command in ('list', 'l', 'ls'):
+            plugins = ((
+                        (p.__name__[:PRINTINGS_PLUGINS_MAX_WIDTH] 
+                         + ' ' * (PRINTINGS_PLUGINS_MAX_WIDTH 
+                                  - len(p.__name__[:PRINTINGS_PLUGINS_MAX_WIDTH])
+                                 ) 
+                         + '\t[ACTIVATED]'
+                        )
+                        if p in self.bot else p.__name__
+                       )
+                       for p in config.import_plugins()
+                      )
+            print('\n'.join(plugins))
+        # printing
+        if command in ('print', 'p', 'pr'):
+            for fn in (str(f) for f in self.bot.plugins):
+                print(str(fn))
+        # activation
+        elif command in ('activate', 'a', 'ac'):
+            if len(values) > 1:
+                for name in values:
+                    clss = config.import_plugin(name)
+                    for cls in clss:
+                        self.bot.add_plugin(cls())
+            else:
+                error = 'name'
+        # deactivation
+        elif command in ('deactivate', 'd', 'dc'):
+            if len(values) > 0:
+                try:
+                    for idx in (int(_) for _ in values):
+                        if not self.bot.rmv_index(idx):
+                            print(str(idx) + ' not found !')
+                except ValueError:
+                    error = 'id'
+            else:
+                error = 'id'
+        # error output
+        if error is not None:
+            print('ERROR:', error_code[error])
+
 
 # PREDICATS ###################################################################
 # ACCESSORS ###################################################################
