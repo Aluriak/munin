e25f6b0bfbe85e16691610887ab407f7f3130568
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2016-01-27 17:50:04 +0100

    plugin: improve API + doc + more powerful system for data saving

--- munin/plugin/plugin.py
+++ munin/plugin/plugin.py
@@ -1,16 +1,11 @@
-# -*- coding: utf-8 -*-
-#########################
-#      PLUGINS          #
-#########################
 """
 Definition of the base class Plugin.
 
 Plugins system allow Munin IRC Bot to be fully extendable.
+
 """
 
-#########################
-# IMPORTS               #
-#########################
+import pickle
 import re
 
 
@@ -20,11 +15,19 @@ PRINTINGS_PLUGINS_MAX_WIDTH = 20
 class Plugin:
     """
     Body of a Plugin implementation.
-    Inherit from this class is not useless, but not necessary:
-    some treatments can be facilitate. (notabily automatic import)
+    Inherit from this class is heavily encouraged, as many treatments on plugins
+    (especially the data persistence) are already implemented.
 
-    It just show what is necessary with docstring.
-    Best examples are already created plugins.
+    Some functions are useful to redefine:
+        - do_command, defining the behavior of the module
+        - only_on_explicit_dest, defining if all messages must be treated
+        - debug_data, defining things to print when user want to get an overview of internal data
+        - help, defining the string returned on IRC when help is asked
+        - persistent_data, defining data to be stored
+        - persistent_filename, defining the filename where data is saved
+
+    Best examples of redefinition usage are the already created plugins.
+
     """
     # default REGEX that is used for determined if
     # plugin must be called, and with whitch parameters.
@@ -64,12 +67,6 @@ class Plugin:
         raise NotImplementedError
 
 
-    def filename(self, basename, prefix=SAVE_FILE_PREFIX,
-                 suffix=SAVE_FILE_SUFFIX):
-        """Return the filename of a file with given basename.
-        Prefix and suffix are by default given by Plugin class"""
-        return prefix + basename + suffix
-
     def want_speak(self):
         """Return True iff self have something to say
 
@@ -96,6 +93,53 @@ class Plugin:
         """True if message must be processed only if the bot is the recipient"""
         return True
 
+    @property
+    def debug_data(self):
+        return self.__class__.__name__
+
+    @property
+    def persistent_data(self):
+        """Return the data that must be saved, or None if no persistence needed"""
+        return None
+
+    @persistent_data.setter
+    def persistent_data(self, values):
+        pass  # nothing to do
+
+    @property
+    def persistent_filename(self):
+        """Basename (without extension) of the file used
+        to store data of the plugin
+
+        default is the lowered class name
+
+        """
+        return self.__class__.__name__.lower()
+
+    def filename(self, basename, prefix=SAVE_FILE_PREFIX,
+                 suffix=SAVE_FILE_SUFFIX):
+        """Return the filename of a file with given basename.
+        Prefix and suffix are by default given by Plugin class"""
+        return prefix + basename + suffix
+
+    def save_persistent_data(self):
+        """Get data in filename, save it in the filename"""
+        payload = self.persistent_data
+        if payload is not None:
+            with open(self.filename(self.persistent_filename), 'wb') as fd:
+                pickle.dump(payload, fd)
+
+    def load_persistent_data(self):
+        """Load data in the filename, or use the default_persistant_data()"""
+        try:
+            with open(self.filename(self.persistent_filename), 'rb') as fd:
+                self.persistent_data = pickle.load(fd)
+        except (IOError, EOFError):
+            self.persistent_data = self.default_persistant_data()
+
+    def default_persistant_data(self):
+        """Return the default persistent data"""
+        return None
 
     def __str__(self):
         return (str(self.id) + ': ' + self.__class__.__name__).ljust(PRINTINGS_PLUGINS_MAX_WIDTH)
