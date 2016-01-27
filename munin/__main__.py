c753cb59545d03e23cddf6684f6336cc662b53cf
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2016-01-27 12:02:19 +0100

    main: use configs

--- munin/__main__.py
+++ munin/__main__.py
@@ -1,9 +1,26 @@
-# -*- coding: utf-8 -*-
-#########################
-#       __MAIN__        #
-#########################
+"""
+
+usage:
+    __main__.py [options]
+    __main__.py <configuration> [options]
+
+options:
+    --help, -h      print this doc
+    --version, -v   print the version and quit
+    --channel=STR   IRC channel to connect to
+
+"""
+
+
+import docopt
 from munin.bot import Bot
+from munin.info import VERSION
+from munin.config import configuration
 from munin.control import Control
 
-Control(Bot())
 
+if __name__ == '__main__':
+    config = configuration(docopt_doc=__doc__)
+    print(config)
+    exit()
+    Control(Bot(config))
